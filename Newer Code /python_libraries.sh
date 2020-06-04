09fc905d13317b2a5f47db85716f71daabeeb58a
Landstein <eric@landsteinmail.com>
2020-06-04 12:07:59 -0400

    bash files

---
+++ Newer Code /python_libraries.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "installing requests library"
+pip3 install requests
+echo "installing pandas library"
+pip3 install pandas
+echo "installing datetime library"
+pip3 install datetime
+echo "installing sqlalchemy library"
+pip3 install sqlalchemy
